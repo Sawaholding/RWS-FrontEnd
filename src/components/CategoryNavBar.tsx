ee8468d006bcc2a93c3ce51bb1b99d45fff3a48b
chandle greff <unconfigured@null.spigotmc.org>
2022-06-08 02:37:04 +0200

    Styled everything

--- src/components/CategoryNavBar.tsx
+++ src/components/CategoryNavBar.tsx
@@ -1,8 +1,21 @@
-import '../styles/CategoryNavBar.css'
+import "../styles/CategoryNavBar.css";
+import { useEffect, useState } from "react";
+import { fetchCategories } from "../api";
+import { Category } from "../types";
+import CategoryCard from "./CategoryCard";
 
 export default function CategoryNavBar() {
+  const [categories, setCategories] = useState<Category[]>([]);
 
-    return (
-        <h1>nav bar</h1>
-    )
+  useEffect(() => {
+    fetchCategories().then(setCategories);
+  }, []);
+
+  return (
+    <div className="category-nav-container">
+      {categories.map((category) => {
+        return <CategoryCard key={category.id} category={category} />;
+      })}
+    </div>
+  );
 }
